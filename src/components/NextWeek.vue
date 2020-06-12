60f92769f5d805bc73d43cf15900293e9b61f569
VFryzlova <55710895+VFryzlova@users.noreply.github.com>
2020-06-12 19:01:40 +0200

    Přidány navigační šipky týdne

--- src/components/NextWeek.vue
+++ src/components/NextWeek.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="next_week_container"></div>
+  <div class="next_week_container">
+    <div class="arrow_left"></div>
+  </div>
 </template>
 
 <script>
@@ -13,7 +15,16 @@ export default {
 
 <style>
 .next_week_container{
-   width: 20%;
+  width: 20%;
   background-color: rgb(31, 31, 31);
+  padding: 20px;
+}
+.arrow_left{
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 10px 0 10px 20px;
+  border-color: transparent transparent transparent white;
+  margin: 0 auto;
 }
 </style>
--- src/components/PreviousWeek.vue
+++ src/components/PreviousWeek.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="previous_week_container"></div>
+  <div class="previous_week_container">
+    <div class="arrow_right"></div>
+  </div>
 </template>
 
 <script>
@@ -13,7 +15,16 @@ export default {
 
 <style>
 .previous_week_container{
-   width: 20%;
+  width: 20%;
   background-color: rgb(31, 31, 31);
+  padding: 20px;
+}
+.arrow_right{
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 10px 20px 10px 0;
+  border-color: transparent white transparent transparent;
+  margin: 0 auto;
 }
 </style>
