05610d6f0ab63477e1afbd884c83ae3c653e8a71
martinrajdl <martin.rajdl@pipedrive.com>
2020-06-17 18:58:57 +0200

    add dev dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
-  <Header />
-    <div class="week_container">
-      <ArrowButton v-bind:direction="'left'" />
-      <CurrentWeek />
-      <ArrowButton v-bind:direction="'right'" />
+    <div>
+        <Header />
+        <div class="week_container">
+            <ArrowButton v-bind:direction="'left'" />
+            <CurrentWeek />
+            <ArrowButton v-bind:direction="'right'" />
+        </div>
+        <ul>
+            <li v-for="day in days" :key="day">
+                <Day v-bind:date="day" />
+            </li>
+        </ul>
     </div>
-    <ul>
-      <li v-for="day in days" :key="day">
-        <Day v-bind:date="day"/>
-      </li>
-    </ul>
-  </div>
 </template>
 
 <script>
@@ -19,34 +19,39 @@ import Header from "./components/Header.vue"
 import CurrentWeek from "./components/CurrentWeek.vue"
 import ArrowButton from "./components/ArrowButton.vue"
 import Day from "./components/Day.vue"
+import moment from "moment"
+
+console.log(moment().format())
 
 export default {
-  name: 'App',
-  data (){
-    return{
-      days: [new Date(2020,6,17), new Date(2020,6,17), new Date(2020,6,17)]
-    }
-  },
-  components: {
-    Header: Header,
-    CurrentWeek: CurrentWeek,
-    ArrowButton: ArrowButton,
-    Day: Day
-  }
-};
-
+    name: "App",
+    data() {
+        return {
+            days: [
+                new Date(2020, 6, 17),
+                new Date(2020, 6, 17),
+                new Date(2020, 6, 17),
+            ],
+        }
+    },
+    components: {
+        Header: Header,
+        CurrentWeek: CurrentWeek,
+        ArrowButton: ArrowButton,
+        Day: Day,
+    },
+}
 </script>
 
 <style>
-body{
-  margin: 0;
-  padding: 0;
-  font-family: sans-serif;
-  background-color: rgb(31, 31, 31);
-  color: white;
+body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background-color: rgb(31, 31, 31);
+    color: white;
 }
-.week_container{
-  display: flex;
+.week_container {
+    display: flex;
 }
-
 </style>
