f73b1c5fcd589671c701cff9bf843f45e5ead882
Iva <ivajuzova@gmail.com>
2020-06-19 19:29:52 +0200

    date format changed

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -2,7 +2,7 @@
     <div class="wrapper">
         <div class="dayContainer">
             <div class="date dayContainerColumn">
-                {{ date.locale("cs").format("MMM Do YY") }}
+                {{ date.locale("cs").format("LLL") }}
             </div>
             <div class="dayContainerRow">
                 <div class="startTime dayContainerColumn">
