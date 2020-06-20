ff7fc50abdcf0d80008c854beff5317b4f477508
Iva <ivajuzova@gmail.com>
2020-06-20 19:55:36 +0200

    rfactoring

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -65,20 +65,19 @@ export default {
         updateStartTime(event) {
             const totalHours = this.getTotalHours()
 
-            const endTime = moment(
-                `${this.date.format("YYYY-MM-DD")} ${this.endTime}`
-            ).format("x")
+            const endTime = this.formatTimeToDbFormat(this.endTime)
 
-            const startTime = moment(
-                `${this.date.format("YYYY-MM-DD")} ${event.target.value}`
-            ).format("x")
+            const startTime = this.formatTimeToDbFormat(event.target.value)
 
+            this.updateDayRecordinFirebase({ startTime, endTime, totalHours })
+        },
+        updateDayRecordinFirebase({ startTime, endTime, totalHours }) {
             db.collection("workDays")
                 .doc(this.documentId)
                 .set({
-                    totalHours: totalHours,
-                    startTime: startTime,
-                    endTime: this.endTime,
+                    totalHours,
+                    startTime,
+                    endTime,
                 })
                 .then(function () {
                     console.log("Document successfully written!")
@@ -89,27 +88,28 @@ export default {
         },
         updateEndTime() {
             const totalHours = this.getTotalHours()
-            const startTime = moment(
-                `${this.date.format("YYYY-MM-DD")} ${this.startTime}`
-            ).format("x")
+            const startTime = this.formatTimeToDbFormat(this.startTime)
 
-            const endTime = moment(
-                `${this.date.format("YYYY-MM-DD")} ${event.target.value}`
-            ).format("x")
+            const endTime = this.formatTimeToDbFormat(event.target.value)
 
-            db.collection("workDays")
-                .doc(this.documentId)
-                .set({
-                    totalHours: totalHours,
-                    startTime: startTime,
-                    endTime: endTime,
-                })
-                .then(function () {
-                    console.log("Document successfully written!")
-                })
-                .catch(function (error) {
-                    console.error("Error writing document: ", error)
-                })
+            this.updateDayRecordinFirebase({ startTime, endTime, totalHours })
+        },
+        updateDayDataFromFirebase(dayRecord) {
+            if (dayRecord.startTime) {
+                this.startTime = moment(dayRecord.startTime, "x").format(
+                    "HH:mm"
+                )
+            }
+            if (dayRecord.endTime) {
+                this.endTime = moment(dayRecord.endTime, "x").format("HH:mm")
+            }
+
+            this.totalHours = dayRecord.totalHours
+        },
+        formatTimeToDbFormat(time) {
+            return moment(`${this.date.format("YYYY-MM-DD")} ${time}`).format(
+                "x"
+            )
         },
     },
     created() {
@@ -118,21 +118,8 @@ export default {
             .doc(this.documentId)
             .onSnapshot((doc) => {
                 if (doc.exists) {
-                    const fireDay = doc.data()
-                    if (fireDay.startTime) {
-                        console.log("neco", fireDay.startTime)
-                        const b = moment(fireDay.startTime, "x").format("HH:mm")
-                        console.log(b)
-                        this.startTime = b
-                    }
-                    if (fireDay.endTime) {
-                        this.endTime = moment(fireDay.endTime, "x").format(
-                            "HH:mm"
-                        )
-                    }
-
-                    console.log({ totalHours: fireDay.totalHours })
-                    this.totalHours = fireDay.totalHours
+                    const dayRecord = doc.data()
+                    this.updateDayDataFromFirebase(dayRecord)
                 }
             })
     },
